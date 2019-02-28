5a65b781bf10f4d08fd2b9ad9f524ed54f153134
Yang Song <yangsong@nat-oitwireless-inside-vapornet100-10-8-10-147.princeton.edu>
2019-02-28 16:39:56 -0500

    replaced router files

--- assignments/assignment2/DVrouter.py
+++ assignments/assignment2/DVrouter.py
@@ -1,44 +1,95 @@
 ####################################################
 # DVrouter.py
-# Names:
-# NetIds:
+# Names: Yang Song, Zhuo Qun Song
+# NetIds: yangsong, zsong
 #####################################################
 
 import sys
-from collections import defaultdict
+from collections import defaultdict, namedtuple
 from router import Router
 from packet import Packet
 from json import dumps, loads
 
+def _table_string(routingTable):
+	"""Turn routing table into string format."""
+	result = []
+	for address in routingTable:
+		cost = routingTable[address]['cost']
+		port = routingTable[address]['port']
+		result.append('{}%{}&{}'.format(address, cost, port))
+	return '#'.join(result)
 
 class DVrouter(Router):
-    """Distance vector routing protocol implementation."""
+	"""Distance vector routing protocol implementation."""
 
-    def __init__(self, addr, heartbeatTime):
-        """TODO: add your own class fields and initialization code here"""
-        Router.__init__(self, addr)  # initialize superclass - don't remove
+	def __init__(self, addr, heartbeatTime):
+		"""TODO: add your own class fields and initialization code here"""
+		Router.__init__(self, addr)  # initialize superclass - don't remove
+		self.heartbeatTime = heartbeatTime # heartbeat time
+		self.currentTime = 0 # current time
+		self.routingTable = dict() # routing table
+		self.neighbors = set() # set of neighbors
+		# self.packeta = [] # content of packets received (for debugging purposes)
+
+	def handlePacket(self, port, packet):
+		"""TODO: process incoming packet"""
+		if packet.isRouting(): 
+			src = packet.srcAddr # source
+			dst = packet.dstAddr # destination
+			content = packet.content # content
+			# self.packeta.append(content) # for debugging
+			table = content.split('#')
+			for entry in table:
+				router = entry.split('%')[0]
+				if router in self.routingTable: # Update our routing table if less cost
+					dist = self.routingTable[src]['cost']
+					cost = int(entry.split('%')[1].split('&')[0]) + dist
+					port = self.routingTable[src]['port']
+					if cost < self.routingTable[router]['cost']:
+						self.routingTable[router] = {'port': port, 'cost': cost}
+				else: # Add to our routing table
+					# DO ADDRESS STUFF TOO
+					dist = self.routingTable[src]['cost']
+					cost = int(entry.split('%')[1].split('&')[0]) + dist
+					port = self.routingTable[src]['port']
+					self.routingTable[router] = {'port': port, 'cost': cost}
+		else: # packet.isTraceroute(): send packet to destination
+			dst = packet.dstAddr
+			if dst in self.routingTable:
+				port = self.routingTable[dst]['port']
+				self.send(port, packet)
 
 
-    def handlePacket(self, port, packet):
-        """TODO: process incoming packet"""
-        pass
+	def handleNewLink(self, port, endpoint, cost):
+		"""TODO: handle new link"""
+		self.routingTable[endpoint] = {'port': port, 'cost': cost} # add link to routing table
+		self.neighbors.add(endpoint) # add new endpoint to neighbors
+		for router in self.neighbors: # send new routing table to all neighbors
+			if router == self.addr: continue
+			porta = self.routingTable[router]['port']
+			packets = Packet(Packet.ROUTING, self.addr, router, _table_string(self.routingTable))
+			self.send(porta, packets)
+
+	def handleRemoveLink(self, port):
+		"""TODO: handle removed link"""
+		address = None # address corresponds to address of removed port
+		for add in self.neighbors:
+			if self.routingTable[add]['port'] == port:
+				address = add
+
+	def handleTime(self, timeMillisecs):
+		"""TODO: handle current time"""
+		self.currentTime = timeMillisecs # handle current time
+		if self.currentTime % 20 == 0: # periodically send routing table to all neighbors
+			for router in self.neighbors:
+				if router == self.addr: continue
+				porta = self.routingTable[router]['port']
+				packets = Packet(Packet.ROUTING, self.addr, router, _table_string(self.routingTable))
+				self.send(porta, packets)
 
 
-    def handleNewLink(self, port, endpoint, cost):
-        """TODO: handle new link"""
-        pass
-
-
-    def handleRemoveLink(self, port):
-        """TODO: handle removed link"""
-        pass
-
-
-    def handleTime(self, timeMillisecs):
-        """TODO: handle current time"""
-        pass
-
-
-    def debugString(self):
-        """TODO: generate a string for debugging in network visualizer"""
-        return ""
+	def debugString(self):
+		"""TODO: generate a string for debugging in network visualizer"""
+		return "Mirror router: address {}, time {}, heartbeat {}, table {}, neighbors {}, string{}"\
+		.format(self.addr, self.currentTime, self.heartbeatTime, self.routingTable, self.neighbors, \
+			_table_string(self.routingTable))
--- assignments/assignment2/LSrouter.py
+++ assignments/assignment2/LSrouter.py
@@ -1,7 +1,7 @@
 ####################################################
 # LSrouter.py
-# Names:
-# NetIds:
+# Names: Yang Song, Zhuo Qun Song
+# NetIds: yangsong, zsong
 #####################################################
 
 import sys
@@ -10,23 +10,100 @@ from router import Router
 from packet import Packet
 from json import dumps, loads
 
-
 class LSrouter(Router):
     """Link state routing protocol implementation."""
 
     def __init__(self, addr, heartbeatTime):
         """TODO: add your own class fields and initialization code here"""
         Router.__init__(self, addr)  # initialize superclass - don't remove
+        self.heartbeatTime = heartbeatTime # heartbeat time
+        self.currentTime = 0 # current time
+        self.routingTable = dict() # table of routers with their LSPs
+        self.neighbors = [] # list of neighbors [(address, cost, port)]
+        self.confirmed = dict() # final routing table, for dijkstra
+        self.tentative = [] # for dijkstra
+        self.debugtentative = [] # used for debugging only
+        self.packeta = set() # set of routers which self has received info about (for debugging)
+
+
+    def dijkstra(self):
+        """Create confirmed from routing table"""
+        self.confirmed = dict() # reset confirmed
+        self.confirmed[self.addr] = {'cost': 0, 'port': 0} # Add 
+        for neighs in self.neighbors:
+            self.tentative.append(neighs)
+        while self.tentative:
+            self.tentative.sort(key=lambda x: -int(x[1]))
+            lowest = self.tentative[-1]            
+            self.tentative.pop()
+            if lowest[0] in self.confirmed: 
+                continue
+            self.confirmed[lowest[0]] = {'cost': lowest[1], 'port': lowest[2]}
+
+            if lowest[0] not in self.routingTable:
+                pass
+            else: 
+                for neigh in self.routingTable[lowest[0]]['neighs'].keys():
+                    if neigh in self.confirmed: 
+                        continue
+                    elif neigh.isupper(): 
+                        self.tentative.append((neigh, lowest[1]+int(self.routingTable[lowest[0]]['neighs'][neigh]['cost']), lowest[2]))#'''self.routingTable[lowest[0]]['neighs'][neigh]['port']'''))
+                    else: 
+                        self.confirmed[neigh] = {'cost': lowest[1]+int(self.routingTable[lowest[0]]['neighs'][neigh]['cost']), 'port': lowest[2]}#'''self.routingTable[lowest[0]]['neighs'][neigh]['port']'''}
+
+    def create_lsp(self):
+        list_neigh = []
+        for neighbor in self.neighbors:
+            add, cost, port = neighbor
+            list_neigh.append('{}%{}%{}'.format(add, cost, port))
+        neigh = '@'.join(list_neigh)
+        neigh = '{}@{}@{}'.format(self.addr, neigh, self.currentTime)
+        return Packet(Packet.ROUTING, self.addr, add, neigh)
+
+
+    def flood(self, lsp):
+        for neighbor in self.neighbors:
+            add, cost, port = neighbor
+            source = lsp.srcAddr
+            if add != source:
+                self.send(port, lsp)
+        pass    
 
 
     def handlePacket(self, port, packet):
         """TODO: process incoming packet"""
-        pass
+        if packet.isRouting(): # update routing table
+            neighbors = packet.content.split('@')
+            address = neighbors[0]
+            time = neighbors[~0]
+            ns = neighbors[1:-1]
+            self.packeta.add(address)
+            neighs = dict()
+            for n in ns:
+                ad, cost, port = n.split('%')
+                neighs[ad] = {'cost': cost, 'port': port}
+            if address in self.routingTable:
+                if time > self.routingTable[address]['time']:
+                    self.routingTable[address] = {'neighs' : neighs, 'time': time}
+            else:
+                self.routingTable[address] = {'neighs' : neighs, 'time': time}
+            for nexts in self.neighbors:
+                if nexts[0] != port:
+                    if self.currentTime - int(time) < 16000: # flood packets until time limit reached
+                    #if address not in self.confirmed:
+                        self.send(nexts[~0], packet)
+        else: # if packet.isTraceroute(): send to destination
+            dst = packet.dstAddr
+            if dst in self.confirmed: # if destination in our table, then send
+                port = self.confirmed[dst]['port']
+                self.send(port, packet)
 
 
     def handleNewLink(self, port, endpoint, cost):
         """TODO: handle new link"""
-        pass
+        #self.routingTable[endpoint] = {'port': port, 'cost': cost}
+        self.neighbors.append((endpoint,cost,port))
+        self.flood(self.create_lsp())
 
 
     def handleRemoveLink(self, port):
@@ -36,9 +113,15 @@ class LSrouter(Router):
 
     def handleTime(self, timeMillisecs):
         """TODO: handle current time"""
-        pass
+        self.currentTime = timeMillisecs
+        if self.currentTime % 10 == 0: # run dijkstra
+            self.dijkstra()
+            # self.flood(self.create_lsp())
 
 
     def debugString(self):
         """TODO: generate a string for debugging in network visualizer"""
-        return ""
+        self.dijkstra()
+        return "CONFIRM {}, Mirror router: address {}, time {}, heartbeat {}, table {}, neighbors {}, packeta {}, string {}, tentative {}"\
+        .format(self.confirmed, self.addr, self.currentTime, self.heartbeatTime, self.routingTable, self.neighbors, self.packeta, \
+            self.debugtentative, self.tentative)
