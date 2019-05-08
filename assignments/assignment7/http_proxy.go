0899abc0a1110eda1ea730eaebf6b37ee7a4065f
Zhuo Qun Song <zhuoqunsong@gmail.com>
2019-05-08 19:07:34 -0400

    io

--- assignments/assignment7/http_proxy.go
+++ assignments/assignment7/http_proxy.go
@@ -1,8 +1,21 @@
+package main
+
+import "os"
+import "fmt"
+import "strconv"
+
 /*****************************************************************************
  * http_proxy.go                                                                 
- * Names: 
- * NetIds:
+ * Names: Rami Farran, Zhuo Qun Song
+ * NetIds: rfarran, zsong
  *****************************************************************************/
 
  // TODO: implement an HTTP proxy
- 
+
+ // compile with $ go build http_proxy.go
+ // run with ./http_proxy PORT
+
+func main() {
+	port, _ := strconv.Atoi(os.Args[1]);
+	fmt.Println(port)
+}
