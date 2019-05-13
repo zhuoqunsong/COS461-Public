33e32026a44d8fac66f97d808a57fecd60f2feed
Zhuo Qun Song <zhuoqunsong@gmail.com>
2019-05-12 22:43:34 -0400

    started DNS

--- assignments/assignment7/http_proxy_DNS.go
+++ assignments/assignment7/http_proxy_DNS.go
@@ -1,12 +1,109 @@
 /*****************************************************************************
- * http_proxy_DNS.go                                                                 
- * Names: 
- * NetIds:
+ * http_proxy.go                                                                 
+ * Names: Rami Farran, Zhuo Qun Song
+ * NetIds: rfarran, zsong
  *****************************************************************************/
 
-// TODO: implement an HTTP proxy with DNS Prefetching
+ // TODO: implement an HTTP proxy
 
-// Note: it is highly recommended to complete http_proxy.go first, then copy it
-// with the name http_proxy_DNS.go, thus overwriting this file, then edit it
-// to add DNS prefetching (don't forget to change the filename in the header
-// to http_proxy_DNS.go in the copy of http_proxy.go)
+ // compile with $ go build http_proxy.go
+ // run with ./http_proxy PORT
+
+package main
+
+import "os"
+import "fmt"
+import "bufio"
+import "net"
+import "net/http"
+import "time"
+import "golang.org/x/net/html"
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ./http_proxy PORT")
+		return
+	}
+	port := os.Args[1]
+
+	// TODO: handle error
+	ln, err := net.Listen("tcp", ":" + port)
+	if err != nil {
+		fmt.Println("Unable to create server at port " + port)
+		return
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Error in establishing connection")
+		} else {
+			go handleConnection(conn)			
+		}
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer closeConnection(conn)
+	// TODO: handle connection
+	req, err := http.ReadRequest(bufio.NewReader(conn))
+	if err != nil {
+		write500(conn)
+		return
+	}
+	if req.Method != http.MethodGet {
+		write500(conn)
+		return
+	}
+	// Now assume that it is a well-formatted GET request.
+
+	// Reset requestURI
+	req.RequestURI = ""
+	req.URL.Host = req.Host
+	req.URL.Scheme = "http"
+	// fmt.Println(req)
+
+	// Send request onward to server
+	// 10 second timeout
+	dur, _ := time.ParseDuration("10s")
+	client := http.Client{nil, nil, nil, dur}
+	resp, err := client.Do(req)
+	if err != nil {
+		write500(conn)
+		return
+	}
+
+	// intercept response
+	z := html.NewTokenizer(resp.Body)
+	for {
+		t := z.Next()
+		fmt.Println(t)
+		if t == html.ErrorToken {
+			break
+		}
+	}
+
+
+
+	// fmt.Println(resp)
+	err = resp.Write(conn)
+	if err != nil {
+		fmt.Println("Error in writing to existing connection")
+		return
+	}
+}
+
+func write500(conn net.Conn) {
+	_, err := conn.Write([]byte("HTTP/1.1 500 Internal Error\r\n"))
+	if err != nil {
+		fmt.Println("Error in writing to existing connection")
+		return
+	}
+}
+
+func closeConnection(conn net.Conn) {
+	err := conn.Close()
+	if err != nil {
+		fmt.Println("Error in closing connection")
+		return
+	}
+}
